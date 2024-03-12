eb596ac49c5a9e90ec4363bb78307a0df4ab0af9
Vadim13245 <159133092+Vadim13245@users.noreply.github.com>
2024-03-12 23:30:36 +0300

    23456

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+
+const positionalLis = document.querySelectorAll('.item');
+console.log('Number of categories:', positionLis.length)
+positionLis.forEach(elem => { 
+  const titl = elem.firstElementChild;
+  console.log('Category;',titl.innerHTML)
+  const list = elem.lastElementChild.children;
+  console.log('Elements:',list.length)
+})
