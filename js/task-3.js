0efe54dd44ab38177bb1936377789937ae8a0596
Vadim13245 <159133092+Vadim13245@users.noreply.github.com>
2024-03-12 23:16:17 +0300

    23456

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+const txtInput = document.querySelector("#name-input");
+const txtOutput = document.querySelector("#name-output");
+
+txtInput.addEventListener("input", (event) => {
+    const txtCont = event.currentTarget.value.trim()
+
+    txtCont.length > 0 ? txtOutput.textContent = txtCont : txtOutput.textContent = "Anonymous";
+  });
